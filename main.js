cb66f55b1e4ff5a999b8e8e86ce5e17b3146d0ac
Tina <tinabast1987@gmail.com>
2023-04-22 15:13:02 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+(function() {
+  const intervalEl = document.getElementById('interval');
+  const countEl = document.getElementById('count');
+  const displayEl = document.getElementById('display');
+let intervalProcess = null;
+  const run = () => {
+    let currentCount = 0;
+    let interval = parseInt(intervalEl.value) || 500;
+    let count = parseInt(countEl.value) || 8;
+    if (intervalProcess) {
+      clearInterval(intervalProcess);
+    }
+    intervalProcess = setInterval(() => {
+      if (currentCount >= count) {
+        currentCount = 1;
+      } else {
+        currentCount++;
+      }
+      displayEl.innerHTML = currentCount;
+    }, interval);
+  };
+  run();
+  countEl.addEventListener('change', run);
+  intervalEl.addEventListener('change', run);
+})();
